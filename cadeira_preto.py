682fcad9651fd2d362ac7cb79f856c60e3d2f3ca
Daniel <79022860+DanielJRS@users.noreply.github.com>
2025-08-25 20:31:40 -0300

    cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,9 @@
-Adicionando primeiro arquivo python.
+# Em python, o que é um cabeçalho dentro de um arquivo python e para que serve? O que é um docstring e para que serve? Qual a diferença do cabeçalho arquivo para o docstring?
+
+# O cabeçalho são as primeiras linhas de um arquivo Python, geralmente contendo: Comentários com informações sobre o arquivo (autor, data, descrição geral); Imports necessários; Configurações especiais (como encoding) 
+# e serve para documentar o projeto como um todo, adicionar informações relevantes para que auxiliem o desenvolvedor na hora de resolver as problemáticas do código.
+
+# O docstring é uma string especial (entre """) que documenta funções, classes ou módulos específicos. Serve para explicar o que uma função/classe faz e como usar.
+
+# Principal diferença: Cabeçalho: documenta o arquivo inteiro (comentários normais #); Docstring: documenta partes específicas do código (strings """) e pode ser acessada programaticamente com help()
+#O docstring é mais "vivo" - Python consegue lê-lo e exibi-lo quando você pede ajuda sobre uma função.
